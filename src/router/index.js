893ac85cee427fcc2aacdc1c9cba273afaf22a18
Nickmanbear <misterkees@hotmail.com>
2020-05-17 19:05:12 +0200

    fix navigation

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 import BoardOverview from '../views/BoardOverview.vue';
 
 Vue.use(VueRouter);
@@ -8,11 +7,6 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/b',
     name: 'Board overview',
     component: BoardOverview,
   },
