92e7e62d08e6976e4471af4bef409ed5b8d1572b
Unknown <s1107083@student.hsleiden.nl>
2020-05-18 13:10:15 +0200

    test.2

--- src/views/BoardOverview.vue
+++ src/views/BoardOverview.vue
@@ -2,7 +2,7 @@
   <div class="overview">
     <h1>Choose your board</h1>
     <p>this is a counter: {{ counter }}</p>
-    <button v-on:click="counter++">+1</button>
+    <button v-on:click="plusCounter">+1</button>
     <div v-if="boards !== []">
       <BoardPreview v-for="board in boards" :key="board.id" v-bind:board="board"/>
     </div>
@@ -39,6 +39,9 @@ export default {
           // TODO: maak een foutmelding ofzo
           console.log(error);
         });
+    },
+    plusCounter() {
+      this.counter += 1;
     },
   },
 };
