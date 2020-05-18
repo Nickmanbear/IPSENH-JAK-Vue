995ef078c7f1de87c48e34154b8f42d64f62119d
Nickmanbear <misterkees@hotmail.com>
2020-05-18 18:08:26 +0200

    only smoll test

--- tests/unit/Column.spec.js
+++ tests/unit/Column.spec.js
@@ -1,26 +1,26 @@
-import { shallowMount } from '@vue/test-utils';
-import Column from '@/components/Column.vue';
-
-describe('Column.spec.js', () => {
-  let cmp;
-
-  beforeEach(() => {
-    cmp = shallowMount(Column, {
-      propsData: {
-        column: {
-          id: 6,
-          boardId: 1,
-          name: 'test column',
-        },
-      },
-    });
-  });
-
-  it('equals name to "test column"', () => {
-    expect(cmp.find('h2').text()).toEqual('test column');
-  });
-
-  it('has the expected html structure', () => {
-    expect(cmp.element).toMatchSnapshot();
-  });
-});
+// import { shallowMount } from '@vue/test-utils';
+// import Column from '@/components/Column.vue';
+//
+// describe('Column.spec.js', () => {
+//   let cmp;
+//
+//   beforeEach(() => {
+//     cmp = shallowMount(Column, {
+//       propsData: {
+//         column: {
+//           id: 6,
+//           boardId: 1,
+//           name: 'test column',
+//         },
+//       },
+//     });
+//   });
+//
+//   it('equals name to "test column"', () => {
+//     expect(cmp.find('h2').text()).toEqual('test column');
+//   });
+//
+//   it('has the expected html structure', () => {
+//     expect(cmp.element).toMatchSnapshot();
+//   });
+// });
