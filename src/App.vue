02c18f8b3a4ec5375fd81c81e6d1988ae84f45db
Alex <alexiaan.vd@hotmail.com>
2020-05-19 12:50:13 +0200

    First inpression

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,50 @@
 <template>
   <div id="app">
-    <Navigation />
-    <br>
-    <router-view/>
+    <div id="nav">
+      <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
+    </div>
+    <router-view @authenticated="setAuthenticated" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Navigation from '@/components/Navigation.vue';
-
-export default {
-  name: 'Home',
-  components: {
-    Navigation,
-  },
-};
+  export default {
+    name: 'App',
+    data() {
+      return {
+        authenticated: false,
+        mockAccount: {
+          username: "nraboy",
+          password: "password"
+        }
+      }
+    },
+    mounted() {
+      if(!this.authenticated) {
+        this.$router.replace({ name: "login" });
+      }
+    },
+    methods: {
+      setAuthenticated(status) {
+        this.authenticated = status;
+      },
+      logout() {
+        this.authenticated = false;
+      }
+    }
+  }
 </script>
 
-<style lang="scss">
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
-  color: #2c3e50;
-}
+<style>
+  body {
+    background-color: #F0F0F0;
+  }
+  h1 {
+    padding: 0;
+    margin-top: 0;
+  }
+  #app {
+    width: 1024px;
+    margin: auto;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,33 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import BoardOverview from '../views/BoardOverview.vue';
+import LoginComponent from "../views/login.vue"
+import SecureComponent from "../views/secure.vue"
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    redirect: {
+      name: "login"
+    }
   },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginComponent
+  },
+  {
+    path: "/secure",
+    name: "secure",
+    component: SecureComponent
+  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home,
+  // },
   {
     path: '/b',
     name: 'Board overview',
@@ -21,6 +39,7 @@ const routes = [
     name: 'Board',
     component: () => import('../views/Board.vue'),
   },
+
 ];
 
 const router = new VueRouter({
