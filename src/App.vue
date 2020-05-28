5e4eba2d03a5e40b5d7478ed5fe5b8e2cbdd6b29
Alexander van Dam <alexiaan.vd@hotmail.com>
2020-05-28 22:52:40 +0200

    Revert the right css code

--- src/App.vue
+++ src/App.vue
@@ -34,16 +34,15 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
   body {
-    background-color: #F0F0F0;
-  }
-  h1 {
-    padding: 0;
-    margin-top: 0;
+    margin: 0;
   }
   #app {
-    width: 1024px;
-    margin: auto;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /*text-align: center;*/
+    color: #2c3e50;
   }
 </style>
