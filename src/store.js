f1c92c159c44fc20588f56ea2923a5511e9fd9dd
Alexander van Dam <alexiaan.vd@hotmail.com>
2020-05-29 12:45:59 +0200

    Changed from '' to false

--- src/store.js
+++ src/store.js
@@ -7,7 +7,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     status: '',
-    token: localStorage.getItem('token') || '',
+    token: localStorage.getItem('token') || false,
     user: {},
   },
   mutations: {
