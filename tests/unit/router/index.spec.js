da9d20036852796c520c883b35b0987b39082a93
Alex <alexiaan.vd@hotmail.com>
2020-06-03 15:36:34 +0200

    Added new tests

---
+++ tests/unit/router/index.spec.js
@@ -0,0 +1,35 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import App from '@/App.vue';
+import VueRouter from 'vue-router';
+
+import router from '../../../src/router';
+import store from '../../../src/store';
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+
+describe('App', () => {
+  let wrapper;
+  beforeEach(async () => {
+    wrapper = mount(App, {
+      localVue,
+      router,
+      store,
+    });
+  });
+  it('renders a Login component via routing', async () => {
+    router.push('/login');
+    await wrapper.vm.$nextTick();
+
+
+    expect(router.currentRoute.fullPath).toBe('/login');
+  });
+
+  it('renders a Register component via routing', async () => {
+    router.push('/register');
+    await wrapper.vm.$nextTick();
+
+
+    expect(router.currentRoute.fullPath).toBe('/register');
+  });
+});
--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -1,9 +1,20 @@
 import store from '../../src/store';
 
 // let store;
-describe('actions', () => {
+describe('store', () => {
   beforeEach(async () => {
-    // store = this.$store;
+    jest.mock('axios', () => ({
+      create: () => ({
+        post: () => Promise.resolve({
+          headers: {
+            Authentication: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJrZWVzI'
+              + 'iwiYWN0aXZlIjp0cnVlLCJleHAiOjE1OTIwNTE1NzF9.iYx09sA139iJSk60eKLhQEExXbV5iZMtN'
+              + 'TB6ImHb4AShCgRI3I0RYbS7H8UKP24MjLzDSa5ypxyg4Mk5h0nIrA',
+          },
+        }),
+        get: () => Promise.resolve({ data: [] }),
+      }),
+    }));
   });
 
   it('Login user', () => {
@@ -11,10 +22,9 @@ describe('actions', () => {
       username: 'username',
       password: 'password',
     };
-
-
     expect.objectContaining(store.dispatch('login', userData));
   });
+
   it('Register a user', () => {
     const userData = {
       username: 'username',
@@ -26,5 +36,23 @@ describe('actions', () => {
 
   it('Logout a user', () => {
     expect.objectContaining(store.dispatch('logout'));
+  });
+
+  it('getter isloggedin', () => {
+    expect(store.getters.isLoggedIn).toEqual(false);
+  });
+
+  it('getter authStatus', () => {
+    expect(store.getters.authStatus).toEqual('');
+  });
+
+  test('the fetch fails with an error', () => {
+    const userData = {
+      username: 'username',
+      password: 'password',
+    };
+    expect(store.dispatch(
+      'login', userData,
+    )).rejects.toMatch('error');
   });
 });
