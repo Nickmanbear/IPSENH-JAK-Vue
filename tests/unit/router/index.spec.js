294cde44c4287717e5696668c92d34c8029ccedd
Alex <alexiaan.vd@hotmail.com>
2020-06-03 18:33:36 +0200

    New tests

--- tests/unit/router/index.spec.js
+++ tests/unit/router/index.spec.js
@@ -32,4 +32,31 @@ describe('App', () => {
 
     expect(router.currentRoute.fullPath).toBe('/register');
   });
+
+  it('renders a board component via routing', async () => {
+    router.beforeEach((to, from, next) => {
+      if (to.matched.some((record) => record.meta.requiresAuth)) {
+        next();
+      }
+    });
+    wrapper = mount(App, {
+      localVue,
+      router,
+      store,
+    });
+
+    router.push('/board/1');
+    await wrapper.vm.$nextTick();
+
+
+    expect(router.currentRoute.fullPath).toBe('/register');
+  });
+
+  it('renders a logout component via routing', async () => {
+    router.push('/logout');
+    await wrapper.vm.$nextTick();
+
+
+    expect(router.currentRoute.fullPath).toBe('/register');
+  });
 });
